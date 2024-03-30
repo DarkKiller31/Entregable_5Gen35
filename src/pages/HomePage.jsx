231de0e91509bab0f2430343e890e16f5e459228
JuanCTrujillo <150824287+DarkKiller31@users.noreply.github.com>
2024-03-29 21:31:29 -0500

    firts commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,23 +3,24 @@ import './styles/HomePage.css'
 
 const HomePage = () => {
   return (
+    <div className="container">
     <div className="home">
       <img className="home__img" src="./pokedex.png" alt="" />
       <h2 className="home__hi">¡Hi trainer!</h2>
       <p className="home__text">To see the pokemon's information,  tell me your trainer name</p>
       
       <FormTrainer />
-
-      <div className="footer__content">
-        <div className="footer__red">
-        </div>
-        <div className="footer__black">
-          <div className="circle__white">
-            <div className="circle__black"></div>
-          </div>
-        </div>
+    </div>
+    <div className="footer__content">
+    <div className="footer__red">
+    </div>
+    <div className="footer__black">
+      <div className="circle__white">
+        <div className="circle__black"></div>
       </div>
     </div>
+  </div>
+  </div>
   )
 }
 
